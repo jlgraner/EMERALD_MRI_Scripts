4ab082e71c1e8d5bb86a2baf31b30db743e269c4
jlgraner <32852039+jlgraner@users.noreply.github.com>
2018-11-20 11:01:17 -0500

    Initial_fMRI_ROI_Analysis
    Created script to generate ROI averages of fMRI activation.

--- fmri_analysis/run_emerald_fmri_roi_analysis.py
+++ fmri_analysis/run_emerald_fmri_roi_analysis.py
@@ -4,8 +4,13 @@ import logging
 
 this_env = os.environ
 
-input_dir = os.path.join(this_env['EMDIR'], 'Analysis/MRI/{sub}/Func/ses-{ses}/Second_level.feat')
+input_dir = os.path.join(this_env['EMDIR'], 'Analysis/MRI/{sub}/Func/Second_level_allruns.gfeat')
 output_dir = os.path.join(this_env['EMDIR'], 'Analysis/MRI/ROI_Analysis_Output')
+
+output_file_template = 'Group_ROI_means_{cope}.txt'
+
+# roi_dir = os.path.join(this_env['EMDIR'], 'Analysis', 'MRI', 'ROIs')
+roi_dir = os.path.join(this_env['EMDIR'], 'Analysis', 'MRI', 'Test_area', 'ROI_testing')
 
 #for each subject:
 #	1) Locate the dist>flow map
@@ -45,10 +50,79 @@ ses = 'day3'
 #               'sub-EM0240'
 #                ]
 
-subs_to_run = ['sub-EM0001']
+subs_to_run = ['sub-EM0179']
+
+#Create a dictionary of all ROI files.
+#Keys will be region names, values will be full file path/names.
+roi_dict = {}
+#This dictionary will house all the ROI means and input files
+output_dict = {}
+
+cope_labels = {
+               'reapGTflow':'4',
+               'distGTflow':'5'
+               }
+
+print('---------------------------------------------------')
+print('Participants to run: {}'.format(subs_to_run))
+print('Cope labels to run: {}'.format(cope_labels.keys()))
+
+if not os.path.exists(roi_dir):
+    print('ROI input directory cannot be found! {}'.format(roi_dir))
+    raise RuntimeError
+else:
+    for element in os.listdir(roi_dir):
+        file_parts = element.split('_')
+        if (len(file_parts)==4) and (file_parts[0]=='ROI') and (file_parts[-1]=='resamp.nii.gz'):
+            #We've found an ROI image file
+            print('Found ROI file: {}'.format(element))
+            roi_dict[file_parts[1]] = os.path.join(roi_dir, element)
 
 for sub in subs_to_run:
-	#Make sure the input directory is there
-	if not os.path.exists(input_dir.format(sub=sub, ses=ses))
-		logging.error('Input directory cannot be found for subject {}!'.format(sub))
-		raise RuntimeError
+    #Make sure the input directory is there
+    if not os.path.exists(input_dir.format(sub=sub, ses=ses)):
+        print('Input directory cannot be found for subject {}! {}'.format(sub,input_dir.format(sub=sub,ses=ses)))
+        raise RuntimeError
+
+    #Look for the output for cope4 (reap>flow) and cope5 (dist>flow)
+    for key in cope_labels:
+        cope_file = os.path.join(input_dir.format(sub=sub,ses=ses), 'cope{}.feat'.format(cope_labels[key]), 'stats', 'cope1.nii.gz')
+        if not os.path.exists(cope_file):
+            print('Input cope image cannot be found! Sub: {sub}; File: {file}'.format(sub=sub,file=cope_file))
+            raise RuntimeError
+
+#If we get here, everything is ready to go
+for sub in subs_to_run:
+    output_dict[sub] = {}
+    for cope in cope_labels:
+        output_dict[sub][cope] = {}
+        cope_file = os.path.join(input_dir.format(sub=sub,ses=ses), 'cope{}.feat'.format(cope_labels[cope]), 'stats', 'cope1.nii.gz')
+        output_dict[sub][cope]['file'] = cope_file
+        for roi in roi_dict:
+            call_parts = [
+                          '3dROIstats',
+                          '-nzmean',
+                          '-nomeanout',
+                          '-quiet',
+                          '-mask',
+                          roi_dict[roi],
+                          cope_file
+                          ]
+            #Save the ROI mean, as a string, into the dictionary
+            output_dict[sub][cope][roi] = subprocess.check_output(call_parts).strip()
+
+#Write the ROI means into .txt files
+for cope in cope_labels:
+    lines_to_write = ['sub\troi\tmean']
+    output_file = os.path.join(output_dir, output_file_template.format(cope=cope))
+    for roi in roi_dict:
+        for sub in subs_to_run:
+            lines_to_write.append('{sub}\t{roi}\t{mean}'.format(sub=sub,roi=roi,mean=output_dict[sub][cope][roi]))
+    print('Writing file: {}'.format(output_file))
+    with open(output_file, 'w') as fd:
+        for line in lines_to_write:
+            fd.write(line+'\n')
+
+
+print('Done')
+print('---------------------------------------------------')
--- fmri_analysis/run_emerald_fmriprep.py
+++ fmri_analysis/run_emerald_fmriprep.py
@@ -10,7 +10,7 @@ output_dir = os.path.join(this_env['EMDIR'], 'Data/MRI/BIDS/new_fmriprep')
 fs_license = '/usr/local/freesurfer/license.txt'
 
 subs_to_run = [
-              'sub-EM0001',
+              # 'sub-EM0001',
               'sub-EM0033',
               'sub-EM0036',
               'sub-EM0038',
@@ -33,46 +33,60 @@ subs_to_run = [
               'sub-EM0219',
               'sub-EM0220',
               'sub-EM0223',
-              'sub-EM0240'
+              'sub-EM0240',
+              'sub-EM0291'
                ]
 
 
-# for sub in subs_to_run:
-call_parts = [
-              'docker',
-              'run',
-              '-it',
-              # '-m',
-              # '20g',
-              '--rm',
-              '-v',
-              '{}:/data:ro'.format(input_dir),
-              '-v',
-              '{}:/out'.format(output_dir),
-              '-v',
-              '{}:/opt/freesurfer/license.txt'.format(fs_license),
-              'poldracklab/fmriprep:latest',
-              '/data',
-              '/out',
-              'participant',
-              '--task', 'emoreg',
-              '--use-aroma',
-              '--ignore-aroma-denoising-errors',
-              '--output-space',
-              'template',
-              '--template-resampling-grid',
-              'native',
-              '--nthreads',
-              '4',
-              # '--low-mem',
-              # '-vvv',
-              '--fs-no-reconall',
-              '--participant_label'
-              ]
+good_runs = []
+bad_runs = []
 
 for sub in subs_to_run:
-  call_parts.append(sub)
+       call_parts = [
+                     'docker',
+                     'run',
+                     '-it',
+                     # '-m',
+                     # '20g',
+                     '--rm',
+                     '-v',
+                     '{}:/data:ro'.format(input_dir),
+                     '-v',
+                     '{}:/out'.format(output_dir),
+                     '-v',
+                     '{}:/opt/freesurfer/license.txt'.format(fs_license),
+                     'poldracklab/fmriprep:latest',
+                     '/data',
+                     '/out',
+                     'participant',
+                     '--task', 'emoreg',
+                     '--use-aroma',
+                     '--ignore-aroma-denoising-errors',
+                     '--output-space',
+                     'template',
+                     '--template-resampling-grid',
+                     'native',
+                     '--nthreads',
+                     '4',
+                     '--fs-no-reconall',
+                     '--participant_label',
+                     sub
+                     ]
 
-print('Calling: {}'.format(string.join(call_parts)))
-subprocess.call(call_parts)
+       print('Calling: {}'.format(string.join(call_parts)))
+       error_flag = subprocess.call(call_parts)
+
+       if error_flag:
+              print('-------------------------------------')
+              print('Subject failed to run: {}'.format(sub))
+              print('-------------------------------------')
+              bad_runs.append(sub)
+       else:
+              good_runs.append(sub)
+
+print('--------------------------------------')
+print('good_runs: {}'.format(good_runs))
+print('\n')
+print('bad_runs: {}'.format(bad_runs))
+print('--------------------------------------')
 print('Done')
