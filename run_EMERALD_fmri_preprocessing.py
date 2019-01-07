9b1bd79f611bd714d9c40ab0369087bc23214695
jlgraner <32852039+jlgraner@users.noreply.github.com>
2019-01-07 15:19:30 -0500

    First Draft of Combined fMRI Preprocessing Script
    First (untested) version of run_EMERALD_fmri_preprocessing, that should theoretically be able to start with BIAC data and go through everything before first-level analysis for fMRI EMERALD data.

--- run_EMERALD_fmri_preprocessing.py
+++ run_EMERALD_fmri_preprocessing.py
@@ -2,7 +2,10 @@
 
 import os, sys
 import logging
+import subprocess
+import string
 import json
+import pandas, numpy
 
 subs_to_run = ['EM0240']
 runs_to_run = ['1', '2', '3', '4']
@@ -11,9 +14,15 @@ this_env = os.environ
 here = os.path.dirname(os.path.realpath(__file__))
 fmri_lib_dir = os.path.join(here, 'fmri_analysis')
 sys.path.append(fmri_lib_dir)
+bxh2bids_dir = os.path.join(this_env['MYDIR'], 'Data', 'bxh2bids')
+sys.path.append(bxh2bids_dir)
+pick_confounds_dir = os.path.join(this_env['MYDIR'], 'Data', 'Graner_BIDS_tools', 'fmriprep_related')
+sys.path.append(pick_confounds_dir)
 
 #Import local libraries
 import emerald_physio_lib
+import bxh2bids as b2b
+import fmriprep_pick_confounds as fpc
 
 em2biac_file = os.path.join(this_env['EMDIR'], 'Scripts', 'MRI_Analysis', 'EM_to_BIAC.json')
 
@@ -73,7 +82,158 @@ for emsub in subs_to_run:
             ecc.csv2fsl(input_file=file_to_run, output_dir=fsl_output_dir)
 
             #--Put data into BIDS format
+            hopes_dreams_file = os.path.join(this_env['EMDIR'] 'Data', 'MRI', 'Orig', 'bxh2bids_hopes_dreams.json')
+            #Read in the hopes and dreams file
+            with open(hopes_dreams_file) as fd:
+                hopes_dreams = json.loads(fd.read())
+            #Pull out some variables
+            source_study_dir = hopes_dreams['source_study_dir']
+            target_study_dir = hopes_dreams['target_study_dir']
+            ses_info_dir = hopes_dreams['ses_info_dir']
+            ses_files = hopes_dreams['ses_files']
+            log_dir = hopes_dreams['log_dir']
+
+            dataid = biacsub
+            ses_info_file = os.path.join(ses_info_dir, hopes_dreams['ses_files'][dataid])
+            with open(ses_info_file) as fd:
+                ses_dict = json.loads(fd.read())
+
+            try:
+                b2b.multi_bxhtobids(dataid, ses_dict, source_study_dir, target_study_dir, log_dir)
+            except:
+                logging.error('Conversion to BIDS failed!')
+                runs_that_failed.append([emsub, biacsub, run, 'bids conversion'])
+                raise RuntimeError
+
+            #--Run fmriqc
+            input_dir = os.path.join(this_env['EMDIR'], 'Data', 'MRI', 'BIDS', 'EMERALD')
+            output_dir = os.path.join(this_env['EMDIR'], 'Data', 'MRI', 'BIDS', 'mriqc')
+
+            call_parts = [
+                          'docker',
+                          'run',
+                          '-it',
+                          '--rm',
+                          '-v',
+                          '{}:/data:ro'.format(input_dir),
+                          '-v',
+                          '{}:/out'.format(output_dir),
+                          'poldracklab/mriqc:latest',
+                          '/data',
+                          '/out',
+                          'participant',
+                          '--ica',
+                          '--no-sub',
+                          '--participant_label',
+                          'sub-{}'.format(emsub)
+                          ]
+
+            logging.info('Calling: {}'.format(string.join(call_parts)))
+            error_flag = subprocess.call(call_parts)
+            if error_flag:
+                logging.error('fmriqc call failed!')
+                runs_that_failed.append([emsub, biacsub, run, 'fmriqc'])
+                raise RuntimeError
+
+            #--Run fmriprep
+            output_dir = os.path.join(this_env['EMDIR'], 'Data', 'MRI', 'BIDS', 'new_fmriprep')
+            fs_license = '/usr/local/freesurfer/license.txt'
+            call_parts = [
+                     'docker',
+                     'run',
+                     '-it',
+                     '--rm',
+                     '-v',
+                     '{}:/data:ro'.format(input_dir),
+                     '-v',
+                     '{}:/out'.format(output_dir),
+                     '-v',
+                     '{}:/opt/freesurfer/license.txt'.format(fs_license),
+                     'poldracklab/fmriprep:latest',
+                     '/data',
+                     '/out',
+                     'participant',
+                     '--task', 'emoreg',
+                     '--use-aroma',
+                     '--ignore-aroma-denoising-errors',
+                     '--output-space',
+                     'template',
+                     '--template-resampling-grid',
+                     'native',
+                     '--nthreads',
+                     '4',
+                     '--fs-no-reconall',
+                     '--participant_label',
+                     'sub-{}'.format(emsub)
+                     ]
+
+            logging.info('Calling: {}'.format(string.join(call_parts)))
+            error_flag = subprocess.call(call_parts)
+            if error_flag:
+                logging.error('fmriprep call failed!')
+                runs_that_failed.append([emsub, biacsub, run, 'fmriprep'])
+                raise RuntimeError
+
+            #--Create confound files for FSL
+            rows_to_remove = 4
+            output_suffix = '_forFSL'
+            confound_dir = os.path.join(this_env['EMDIR'], 'Data/MRI/BIDS/new_fmriprep/fmriprep/sub-{}/ses-day3/func/'.format(emsub))
+            confound_name = 'sub-{}_ses-day3_task-emoreg_run-0{}_desc-confounds_regressors.tsv'.format(emsub, run)
+            include_tr_motcen_regs = 1
+            mot_cen_limit = 0.2
+
+            confounds_to_include = ['csf', 'white_matter', 'dvars', 'framewise_displacement', 't_comp_cor',
+                                    'a_comp_cor', 'trans_x', 'trans_y', 'trans_z', 'rot_x', 'rot_y', 'rot_z',
+                                    'CSF', 'WhiteMatter', 'stdDVARS', 'FramewiseDisplacement', 'tCompCor',
+                                    'aCompCor', 'X', 'Y', 'Z', 'RotX', 'RotY', 'RotZ']
+
+            confound_file = os.path.join(confound_dir, confound_name)
+
+            logging.info('Creating confound file from: {}'.format(confound_file))
+            output_name = os.path.split(confound_file)[-1][:-4]+str(output_suffix)
+            output_file = os.path.join(confound_dir, output_name+'.tsv')
+            logging.info('Output file name set: {}'.format(output_file))
+
+            if not os.path.exists(confound_file):
+                logging.error('Confound file cannot be found: {}'.format(confound_file))
+                runs_that_failed.append([emsub, biacsub, run, 'confound input file missing'])
+                raise RuntimeError
+            #Create a list of confound column names to include in the new file
+            include_list = [] #This list will contain the specific names of columns to be included in the output
+            data = pandas.read_csv(confound_file, sep='\t', engine='python', dtype=float)
+            #Create an empty data frame to fill in
+            new_data = pandas.DataFrame()
+            for element in confounds_to_include:
+                #Deal with labels with more than one confound column
+                if element in ['t_comp_cor', 'a_comp_cor', 'cosine', 'non_steady_state_outlier',
+                               'aroma_motion', 'tCompCor', 'aCompCor', 'Cosine', 'NonSteadyStateOutlier', 'AROMA']:
+                    #Find column header names that contain the label category
+                    match_list = fpc.match_columns(data, element)
+                    print('Confound label matched with list: {}'.format(match_list))
+                    #Add them all to the full list
+                    include_list = include_list + match_list
+                #Deal with labels that don't have multiple confound columns
+                else:
+                    include_list.append(element)
+            #Put the columns with the included column labels into the new data frame
+            new_data = fpc.add_columns(data, new_data, include_list)
+            #If desired, create single-TR regressors and add them to the new data frame
+            if include_tr_motcen_regs:
+                mot_censor_dataframe = fpc.create_motion_censor_regs(data, mot_cen_limit, rows_to_remove)
+                new_data = fpc.add_columns(mot_censor_dataframe, new_data, mot_censor_dataframe.keys())
+            #If desired, remove initial entries corresponding to pre-steady-state TRs
+            if rows_to_remove > 0:
+                new_data = new_data.drop(range(rows_to_remove))
+            #Write the new data frame out as a new confound file
+            logging.info('Writing confounds output file: {}'.format(output_file))
+            new_data.to_csv(path_or_buf=output_file, sep='\t', index=False)
 
             runs_that_ran.append([emsub, biacsub, run])
         except:
+            logging.error('Top-level exception caught for subject {}, run {}.'.format(emsub, run))
+
+logging.info('--------------------------------------------')
+logging.info('Runs that ran: {}'.format(runs_that_ran))
+logging.info('Runs that failed: {}'.format(runs_that_failed))
+logging.info('--------------------------------------------')
 
