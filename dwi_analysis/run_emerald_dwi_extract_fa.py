58f31ca2d37444772e444d01096e323939ee36fd
jlgraner <32852039+jlgraner@users.noreply.github.com>
2019-02-08 10:29:00 -0500

    Initial creation of code to extract tract FA
    Started run_emerald_dwi_extract_fa.py, which will automatically extract average FA values from tract ROIs.

---
+++ dwi_analysis/run_emerald_dwi_extract_fa.py
@@ -0,0 +1,31 @@
+
+
+import os, shutil
+import subprocess
+import pandas
+
+
+this_env = os.environ
+
+sub_list = [
+            'EM0001'
+            ]
+
+tract_list = [
+              'L_SLFII',
+              'R_SLFII',
+              'L_Unc',
+              'R_Unc'
+              ]
+
+bad_runs = []
+good_runs = []
+
+base_output_dir = os.path.join(this_env['EMDIR'], 'Analysis', 'MRI', 'DTI_FA_02082019')
+#If the output directory does not exist, create it
+if not os.path.exists(base_output_dir):
+    print('Output directory not found; creating it: {}'.format(base_output_dir))
+    os.mkdir(base_output_dir)
+
+output_file = os.path.join(base_output_dir, 'DTI_tract_FAs_02082019.csv')
+
