6b87b7b70a79c1c42fc0d8618426fac52bae00d2
jlgraner <32852039+jlgraner@users.noreply.github.com>
2020-01-16 12:53:07 -0500

    Update run_create_ppi_evs.py
    Major changes to run_create_ppi_evs.py. It now creates time-courses based on peak-sphere ROIs based on predefined, prerun contrasts.

--- fsl_analysis/run_create_ppi_evs.py
+++ fsl_analysis/run_create_ppi_evs.py
@@ -14,13 +14,13 @@ import pandas
 
 this_env = os.environ
 
-run_list = ['1', '2', '3', '4']
-# run_list = ['2', '3', '4']
+# run_list = ['1', '2', '3', '4']
+run_list = ['1']
 
-masks_to_apply = ['amy', 'dACC', 'dlPFC', 'infPar', 'vlPFC', 'vmPFC']
-# masks_to_apply = ['amy']
+# masks_to_apply = ['amy', 'dACC', 'dlPFC', 'infPar', 'vlPFC', 'vmPFC']
+masks_to_apply = ['amy', 'dlPFCsphere', 'vlPFCsphere']
 
-contrasts_to_make = [ ['diststrategycues', 'negflowstrategycues'], ['reapstrategycues', 'negflowstrategycues'] ]
+# contrasts_to_make = [ ['diststrategycues', 'negflowstrategycues'], ['reapstrategycues', 'negflowstrategycues'] ]
 
 subs_to_run = ['EM0569']
 
@@ -67,8 +67,11 @@ condition_dir = os.path.join(this_env['EMDIR'], 'Analysis', 'MRI', 'sub-{sub}', 
 image_dir = os.path.join(this_env['EMDIR'], 'Data', 'MRI', 'BIDS', 'fmriprep', 'sub-{sub}', 'ses-day3', 'func')
 
 #Directory containing masks to apply
-# mask_dir = os.path.join(this_env['EMDIR'], 'Analysis', 'MRI', 'ROIs')
-mask_dir = os.path.join(this_env['EMDIR'], 'Analysis', 'MRI', 'sub-{sub}', 'Func', 'Intensity_Masked_ROIs')
+# mask_dir = os.path.join(this_env['EMDIR'], 'Analysis', 'MRI', 'sub-{sub}', 'Func', 'Intensity_Masked_ROIs')
+mask_file = os.path.join(this_env['EMDIR'], 'Analysis', 'MRI', 'sub-{sub}', 'Func', 'Second_level_allruns.gfeat', 'cope{cope}.feat', 'featquery_{roi}_output', 'mask.nii.gz')
+
+#Directory containing activation-based sphere masks
+# sphere_mask = os.path.join(this_env['EMDIR'], 'Analysis', 'MRI', 'sub-{sub}', 'Func', 'Second_level_allruns.gfeat', 'cope{cope}.feat', 'featquery_{roi}_output', 'mask.nii.gz')
 
 #Directory in which to write the new EV files
 output_dir = os.path.join(this_env['EMDIR'], 'Analysis', 'MRI', 'sub-{sub}', 'Func', 'Condition_files')
@@ -84,10 +87,15 @@ for sub in subs_to_run:
         #Extract ROI mask time-courses and save them to files
         try:
             for mask in masks_to_apply:
+                if mask == 'dlPFCsphere':
+                  cope = '5'
+                else:
+                  cope = '4'
+
                 print('Extracting ROI time-course for mask: {}'.format(mask))
                 fmri_image = os.path.join(image_dir.format(sub=sub), 'sub-{}_emoreg_run{}_AROMApreproc_short_tempfilt_brain.nii.gz'.format(sub,run))
-                # mask_image = os.path.join(mask_dir, 'ROI_{}_final.nii.gz'.format(mask))
-                mask_image = os.path.join(mask_dir.format(sub=sub), 'sub_{sub}_ROI_{mask}_final.nii.gz'.format(sub=sub,mask=mask))
+                # mask_image = os.path.join(mask_dir.format(sub=sub), 'sub_{sub}_ROI_{mask}_final.nii.gz'.format(sub=sub,mask=mask))
+                mask_image = mask_file.format(sub=sub, cope=cope, roi=mask)
                 output_file = os.path.join(output_dir.format(sub=sub), 'sub-{sub}_emoreg_run{run}_AROMApreproc_short_{mask}_timecourse.txt'.format(sub=sub, run=run, mask=mask))
 
                 call_parts = ['3dROIstats',
@@ -104,34 +112,34 @@ for sub in subs_to_run:
                     fid.write('{}'.format(call_output.decode("utf-8").replace('\t','')))
 
             #Put together contrast EVs for PPI
-            for first, second in contrasts_to_make:
-                #Contrast EVs will be created setting the third column of the
-                #"first" events to 1 and the third column of the "second" events
-                #to -1.
+            # for first, second in contrasts_to_make:
+            #     #Contrast EVs will be created setting the third column of the
+            #     #"first" events to 1 and the third column of the "second" events
+            #     #to -1.
 
-                print('Putting together PPI contrast regressor for conditions:')
-                print('Condition 1: {}'.format(first))
-                print('Condition 2: {}'.format(second))
-                #Create dataframe that will hold both sets of timings for a while
-                appended_data = pandas.DataFrame(columns=['onset','duration','weight'])
+            #     print('Putting together PPI contrast regressor for conditions:')
+            #     print('Condition 1: {}'.format(first))
+            #     print('Condition 2: {}'.format(second))
+            #     #Create dataframe that will hold both sets of timings for a while
+            #     appended_data = pandas.DataFrame(columns=['onset','duration','weight'])
 
-                #Read in the contents of the first file
-                first_file = os.path.join(condition_dir.format(sub=sub), '{sub}_run{run}_{cond}_fslconds.txt'.format(sub=sub, run=run, cond=first))
-                first_data = pandas.read_csv(first_file, sep='\t', names=['onset','duration','weight'], engine='python')
-                appended_data = pandas.concat([appended_data, first_data], ignore_index=True)
+            #     #Read in the contents of the first file
+            #     first_file = os.path.join(condition_dir.format(sub=sub), '{sub}_run{run}_{cond}_fslconds.txt'.format(sub=sub, run=run, cond=first))
+            #     first_data = pandas.read_csv(first_file, sep='\t', names=['onset','duration','weight'], engine='python')
+            #     appended_data = pandas.concat([appended_data, first_data], ignore_index=True)
 
-                #Read in the contents of the second file
-                second_file = os.path.join(condition_dir.format(sub=sub), '{sub}_run{run}_{cond}_fslconds.txt'.format(sub=sub, run=run, cond=second))
-                second_data = pandas.read_csv(second_file, sep='\t', names=['onset','duration','weight'], engine='python')
-                second_data.loc[:,'weight'] = -1
-                appended_data = pandas.concat([appended_data, second_data], ignore_index=True)
+            #     #Read in the contents of the second file
+            #     second_file = os.path.join(condition_dir.format(sub=sub), '{sub}_run{run}_{cond}_fslconds.txt'.format(sub=sub, run=run, cond=second))
+            #     second_data = pandas.read_csv(second_file, sep='\t', names=['onset','duration','weight'], engine='python')
+            #     second_data.loc[:,'weight'] = -1
+            #     appended_data = pandas.concat([appended_data, second_data], ignore_index=True)
 
-                #Sort the new event data frame by onset
-                df_sorted = appended_data.sort_values(['onset'])
+            #     #Sort the new event data frame by onset
+            #     df_sorted = appended_data.sort_values(['onset'])
 
-                #Write output file
-                output_file = os.path.join(output_dir.format(sub=sub), '{sub}_run{run}_{first}-{second}_ppi.txt'.format(sub=sub,run=run,first=first,second=second))
-                df_sorted.to_csv(path_or_buf=output_file, sep='\t', index=False, header=False)
+            #     #Write output file
+            #     output_file = os.path.join(output_dir.format(sub=sub), '{sub}_run{run}_{first}-{second}_ppi.txt'.format(sub=sub,run=run,first=first,second=second))
+            #     df_sorted.to_csv(path_or_buf=output_file, sep='\t', index=False, header=False)
                 good_runs.append([sub,run,mask])
         except Exception as err:
             print('Something went wrong for {}, run {}!'.format(sub,run))
