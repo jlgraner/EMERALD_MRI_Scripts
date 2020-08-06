7e763c254e7b661f5b22a45b8b7fcbc8bf6093a6
John Graner <jlg73@ccn-labarserv2.win.duke.edu>
2020-08-06 16:47:58 -0400

    noAROMA run edits

--- fmri_analysis/run_emerald_postfmriprep_noAROMA.py
+++ fmri_analysis/run_emerald_postfmriprep_noAROMA.py
@@ -12,7 +12,7 @@ this_env = os.environ
 overwrite = 1
 skip = 0
 
-subs_to_run = ['EM0192', 'EM0202', 'EM0206']
+subs_to_run = ['EM0291', 'EM0304', 'EM0381']
 
 # subs_to_run = [
 #                # 'EM0033',
